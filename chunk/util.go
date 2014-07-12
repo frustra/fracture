20f37b33c4e1625e05f715263499dc937232b9bd
Justin Li <jli.justinli@gmail.com>
2014-07-11 23:15:17 -0400

    Fix math

--- chunk/util.go
+++ chunk/util.go
@@ -2,10 +2,10 @@ package chunk
 
 func ChunkCoordsToNode(x, z int64) (nx, nz int64) {
 	if x < 0 {
-		x -= ChunkWidthPerNode
+		x += 1 - ChunkWidthPerNode
 	}
 	if z < 0 {
-		z -= ChunkWidthPerNode
+		z += 1 - ChunkWidthPerNode
 	}
 	nx = (x / ChunkWidthPerNode) * ChunkWidthPerNode
 	nz = (z / ChunkWidthPerNode) * ChunkWidthPerNode
