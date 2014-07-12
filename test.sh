b16dce587c17b8e901214262872d2ad96f1165ed
Justin Li <jli.justinli@gmail.com>
2014-07-11 23:58:01 -0400

    Add test start script

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fracture -addr :25565 edge &
+fracture -addr :25566 -node :7947 -x 0 -z 0 chunk &
+fracture -addr :25567 -node :7948 -x -8 -z 0 chunk &
+fracture -addr :25568 -node :7949 -x -8 -z -8 chunk &
+fracture -addr :25569 -node :7950 -x 0 -z -8 chunk &
+fracture -addr :25570 -node :7951 player &
