0b209c57d3588e30d6ea8abb5a90d0dcfdff7fa3
Justin Li <jli.justinli@gmail.com>
2014-07-12 17:51:19 -0400

    Less hardcoding

--- edge/protocol/metadata_test.go
+++ edge/protocol/metadata_test.go
@@ -8,7 +8,7 @@ import (
 
 var MetadataSerializationTests = map[*Metadata][]byte{
 	NewMetadata(ArrowCountID, byte(0x52)): []byte{0x09, 0x52, 0x7f},
-	NewMetadata(HealthID, float32(10.4)):  []byte{0x66, 0x41, 0x26, 0x66, 0x66, 0x7f},
+	NewMetadata(HealthID, float32(10.4)):  []byte{3<<5 | 6, 0x41, 0x26, 0x66, 0x66, 0x7f},
 }
 
 func TestMetadataSerialization(t *testing.T) {
