61e1bec0754baf66ef5514dff2c06b7c3b9ee3ae
Justin Li <jli.justinli@gmail.com>
2014-07-11 23:58:15 -0400

    Autokill

--- test.sh
+++ test.sh
@@ -6,3 +6,5 @@ fracture -addr :25567 -node :7948 -x -8 -z 0 chunk &
 fracture -addr :25568 -node :7949 -x -8 -z -8 chunk &
 fracture -addr :25569 -node :7950 -x 0 -z -8 chunk &
 fracture -addr :25570 -node :7951 player &
+cat
+killall fracture
