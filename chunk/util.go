b42c95334bd3b4cb3d2bc2d21912676e920abb27
Justin Li <jli.justinli@gmail.com>
2014-07-13 01:17:57 -0400

    Add more chunk helper function and tests

--- chunk/util.go
+++ chunk/util.go
@@ -11,3 +11,15 @@ func ChunkCoordsToNode(x, z int64) (nx, nz int64) {
 	nz = (z / ChunkWidthPerNode) * ChunkWidthPerNode
 	return nx, nz
 }
+
+func WorldCoordsToChunk(x, z int64) (cx, cz int64) {
+	if x < 0 {
+		x += 1 - BlockXSize
+	}
+	if z < 0 {
+		z += 1 - BlockZSize
+	}
+	cx = x / BlockXSize
+	cz = z / BlockZSize
+	return cx, cz
+}
---
+++ chunk/util_test.go
@@ -0,0 +1,47 @@
+package chunk_test
+
+import (
+	"testing"
+
+	. "github.com/frustra/fracture/chunk"
+)
+
+var chunkToNodeTests = map[int64]int64{
+	0:  0,
+	5:  0,
+	11: 8,
+	-1: -8,
+	-8: -8,
+	-9: -16,
+}
+
+func TestChunkCoordsToNode(t *testing.T) {
+	for m, expected := range chunkToNodeTests {
+		got1, got2 := ChunkCoordsToNode(m, m)
+
+		if got1 != got2 || got1 != expected {
+			t.Errorf("       got: %d, %d\n  expected: %d, %d", got1, got2, expected, expected)
+		}
+	}
+}
+
+var worldToChunkTests = map[int64]int64{
+	0:   0,
+	15:  0,
+	16:  1,
+	22:  1,
+	-1:  -1,
+	-15: -1,
+	-16: -1,
+	-17: -2,
+}
+
+func TestWorldCoordsToChunk(t *testing.T) {
+	for m, expected := range worldToChunkTests {
+		got1, got2 := WorldCoordsToChunk(m, m)
+
+		if got1 != got2 || got1 != expected {
+			t.Errorf("       got: %d, %d\n  expected: %d, %d", got1, got2, expected, expected)
+		}
+	}
+}
